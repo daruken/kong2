85c69687a6813bb1926c5d4d76cb0af9ea622d58
daruken <>
2023-02-27 20:49:07 +0900

    refactor: 사용자 가입 시 이메일 주소 Validation 체크 로직 추가

--- src/main/java/com/h2/kong2/member/domain/dto/SignUpDto.java
+++ src/main/java/com/h2/kong2/member/domain/dto/SignUpDto.java
@@ -1,10 +1,12 @@
 package com.h2.kong2.member.domain.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 
 @Getter
 public class SignUpDto {
     String name;
     String password;
+    @Email
     String email;
 }
